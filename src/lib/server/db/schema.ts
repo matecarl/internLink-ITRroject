81781c1cb211e467377eceb3f97592f304d5f37d
matecarl <113537267+matecarl@users.noreply.github.com>
2024-12-10 17:24:32 +0100

    Configure local DB connection and create first migration

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,5 @@
 import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	age: integer('age')
+	id: serial('id').primaryKey()
 });
