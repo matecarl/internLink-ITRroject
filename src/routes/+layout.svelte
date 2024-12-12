df0958bcfdb5e514994cb3660d24edea610dd98c
Karim Altir <mhd.altir@student.ehb.be>
2024-12-12 17:43:59 +0100

    adding routing + working with settings ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
-<script lang="ts">
+<script>
 	import '../app.css';
+	import About from './About/+page.svelte';
+	import Home from './+page.svelte';
+	import EditPage from './EditPage/+page.svelte';
+	//import Router from 'svelte-spa-router'
+	import NotFound from './NotFound/+page.svelte';
+	import { Router, Route } from 'svelte-routing';
+
 	let { children } = $props();
 </script>
+
+<div class="navbar bg-neutral text-neutral-content">
+	<ul class="menu menu-horizontal px-1">
+		<li><a href="/">Home</a></li>
+		<li><a href="/About">About</a></li>
+		<li><a href="/EditPage">Edit page</a></li>
+	</ul>
+</div>
 
 {@render children()}
---
+++ src/routes/EditPage/+page.svelte
@@ -0,0 +1,39 @@
+<label class="form-control w-full max-w-xs">
+	<div class="label">
+		<span class="label-text">Name Company</span>
+	</div>
+	<input type="text" placeholder="Enter the name of your company" class="input input-bordered w-full max-w-xs" />
+
+	<div class="label">
+		<span class="label-text">Description</span>
+	</div>
+	<input type="text" placeholder="Enter a description about your company" class="input input-bordered w-full max-w-xs" />
+
+	<div class="label">
+		<span class="label-text">Location</span>
+	</div>
+	<input type="text" placeholder="Enter the location of the company" class="input input-bordered w-full max-w-xs" />
+
+	<div class="label">
+		<span class="label-text">Enternal Links</span>
+	</div>
+
+	<input type="url"  class="input input-bordered w-full max-w-xs" />
+
+	<div class="label">
+		<span class="label-text-alt">Add links to your social profiles or website</span>
+	</div>
+
+
+	<div class="label">
+		<span class="label-text">Representives</span>
+	</div>
+
+	<input type="url" class="input input-bordered w-full max-w-xs" />
+
+	<div class="label">
+		<span class="label-text-alt">Add links to your social profiles or website</span>
+	</div>
+
+	<button class="btn btn-active btn-neutral">change profile</button>
+</label>
